ad05648efba104e4c50932e888a1821d8eb3650e
epruc <epruc@cs.umass.edu>
2018-10-29 23:37:45 -0400

    Added Subtract, Divide, Variables, and Parentheses

--- src/index.js
+++ src/index.js
@@ -174,22 +174,63 @@ function parse(str) {
 
   let num = P.regexp(/[0-9]+/)
     .map(function(str) { return { type: "number", value: Number(str) }; });
-
-  let mul = num.chain(function (lhs) {
+    
+  let name = P.regexp(/[A-Za-z]+/)
+   .map(function(str) { return { type: "variable", value: str }; });
+   
+  let stmt = P.string("let").chain(function(_){
+      return name.chain(function(x){
+          return P.string("=").chain(function(_){
+              return add.chain(function(named_exp){
+                  return P.string(";").chain(function(_){
+                      return P.succeed({ name: 'Variable', named: x, body: named_exp })
+                  })
+              })
+          })
+      }) //name named and body 
+  })
+  let atom = num.or(P.string("(").chain(function(lhs){
+        return add 
+       .skip(P.string(")"))
+    }));
+  
+  let mul = atom.chain(function (lhs) {
     return P.string('*').chain(function (_) {
       return mul.chain(function(rhs) {
         return P.succeed({ type: 'Multiply', left: lhs, right: rhs });
       });
-    }).or(P.succeed(lhs)) ;
+    }).or(P.string("/").chain(function(_){
+        return add.chain(function(rhs) {
+        return P.succeed({ type: 'Divide', left: lhs, right: rhs });
+         });
+  }))
+    .or(P.succeed(lhs)) ;
   });
-
   // 1. let add =  ... same as above, but uses mul instead of num
   // 2. let atom = num.or( "(" add ")")
+  //Psuedocode: Identify "(" and ")" , parse expression between parentheses
 
-
-  return mul.tryParse(str);
+   let add = mul.chain(function (lhs) {
+    return P.string('+').chain(function (_) {
+      return add.chain(function(rhs) {
+        return P.succeed({ type: 'Add', left: lhs, right: rhs });
+      });
+    }).or(P.string("-").chain(function(_){
+        return add.chain(function(rhs) {
+        return P.succeed({ type: 'Subtract', left: lhs, right: rhs });
+         });
+    }))
+    .or(P.succeed(lhs)) ;
+  });  
+  
+  //Do division at the same level as multiplication - complete
+ 
+   return stmt.or(add).tryParse(str); 
+   //stmt.oradd tryParse(str) 
+   
 }
-
+// add variables and conditionals 
+//stmt::= add or let x = add;add 
 ////////////////////////////////////////////end parsimmon math.js/////////////////////////
 
 var calculate = function (node) {
@@ -216,6 +257,11 @@ var calculate = function (node) {
     }
     else if (node.type === "Subtract") {
         return  calculate(node.left) - calculate(node.right);
+        
+    }else if ( node.name === "Variable"){
+        let name = node.named.value; 
+        let value = node.body.value; 
+        return(name + "=" + value);
     }
     else {
       throw 'Unknown type';
@@ -234,7 +280,10 @@ const example2 = {
   right: { type: "number", value: 40 }
 };
 
-console.log(calculate(example2));
-console.log(calculate(parse("123123")));
-
-console.log(parse("123123*232*2*4"));
+//console.log(calculate(example2));
+console.log(calculate(parse("letx=123123;")));
+//console.log(parse("123123*232*2*4"));
+//console.log(parse("123+323+2+4"));
+//console.log(parse("123/3"));
+//console.log(parse("123-3"));
+//console.log(parse("letb=123;"));
