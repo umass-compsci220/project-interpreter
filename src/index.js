92014e3be45da3ed7b594b08b2a8416c561fd635
epruc <epruc@cs.umass.edu>
2018-11-18 16:21:12 -0500

    fixed blank space after variables and numbers

--- src/index.js
+++ src/index.js
@@ -4,34 +4,45 @@ let P = require("parsimmon");
 function token(name_tok){
     return P.seq(P.string(name_tok),P.whitespace.or(P.eof)); 
 }
+//fix variable so that space can follow-complete but ask arjun about "" 
+//multiple statements to work 
+//work on the evaluator - evaluator parses variables 
+//sequence of lets, change the parser so that it returns an array -- returns multiple lets 
+//variable addition occurs at the the same level as numbers 
 
 function operator(name_op){
     return P.seq(P.string(name_op),P.optWhitespace.or(P.eof)); 
 }
 
+function numer(name){
+    return P.seq(name,P.optWhitespace.or(P.eof)).map(function(array){
+        return array[0]; 
+    });
+}
+//add number 
 function parse(str) {
 
   let num = P.regexp(/[0-9]+/)
     .map(function(str) { return { type: "number", value: Number(str) }; });
     
   let name = P.regexp(/[A-Za-z]+/)
-   .map(function(str) { return { type: "variable", value: str }; });
-   
+   .map(function(str) { return { type: "variable", value: str}; });
+  
   let stmt = token("let").chain(function(_){
-      return name.chain(function(x){
-          return operator ("=").chain(function(_){
+      return numer(name).chain(function(x){
+          return operator("=").chain(function(_){
               return add.chain(function(named_exp){
                   return operator(";").chain(function(_){
                       return P.succeed({ name: 'Variable', named: x, body: named_exp })
                   })
               })
           })
-      }) //name named and body 
+      })
   })
-  let atom = num.or(operator("(").chain(function(lhs){
+  let atom = numer(num).or(numer(name).or(operator("(").chain(function(lhs){
         return add 
        .skip(operator(")"))
-    }));
+    })));
   
   let mul = atom.chain(function (lhs) {
     return operator('*').chain(function (_) {
@@ -63,12 +74,11 @@ function parse(str) {
   });  
   
   //Do division at the same level as multiplication - complete
- 
+    //Object.values
    return stmt.or(add).tryParse(str); 
-   //stmt.oradd tryParse(str) 
-   
+   //.many apply the parser multiple times 
 }
-// add variables and conditionals 
+// add variables 
 //stmt::= add or let x = add;add 
 ////////////////////////////////////////////end parsimmon math.js/////////////////////////
 
@@ -97,10 +107,11 @@ var calculate = function (node) {
     else if (node.type === "Subtract") {
         return  calculate(node.left) - calculate(node.right);
         
-    }else if ( node.name === "Variable"){
-        let name = node.named.value; 
-        let value = node.body.value; 
-        return(name + "=" + value);
+    }else if (node.name === "Variable"){
+        (node.named.value) = node.body.value;
+        return(node.named.value);
+        //evaluate the variable to a value 
+        
     }
     else {
       throw 'Unknown type';
@@ -120,10 +131,13 @@ const example2 = {
 };
 
 //console.log(calculate(example2));
-//console.log(calculate(parse("let x=123123;")));
+//console.log(calculate(parse("let x= 123123;")));
 //console.log(parse("123123*232*2*4"));
 //console.log(parse("123+323+2+4"));
 //console.log(parse("123/3"));
 //console.log(parse("123-3"));
-//console.log(parse("let b=123;"));
+console.log((parse("let b = 123;")));
+console.log((parse("b+b")));
 module.exports = parse
+
+//build a dictionary maping x.
